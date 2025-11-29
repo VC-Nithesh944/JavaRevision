ec4fc679b436a98ebb1f29bd080ff27291706c05
Nithesh <nitheshu944@gmail.com>
2025-11-29 23:21:31 +0530

    Add Static Variable Constructor Counter

---
+++ Counter.java
@@ -0,0 +1,19 @@
+public class Counter {
+    static int count = 0;
+
+    Counter(){
+        count++;
+    }
+
+    static void getCount(){
+        System.out.println("The value of Count is : " + count);
+    }
+
+    public static void main(String args[]){
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+        Counter.getCount();
+    }
+}
+
